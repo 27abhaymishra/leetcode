8a2facb4b262804e57c2cd8e67c984a8b7f48ff8
Abhay mishra <126569073+27abhaymishra@users.noreply.github.com>
2025-05-23 21:41:18 +0530

    Time: 0 ms (100%), Space: 42.1 MB (68.21%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,27 +1,32 @@
-class Solution {
-    public int search(int[] nums, int target) {
-    int lo = 0, hi=nums.length-1;
 
-    while(lo <= hi){
-        int mid = (lo + hi) / 2;
+class Solution {  
+    public int search(int[] nums, int target) {  
+        int lo = 0, hi = nums.length - 1;  
 
-        if(nums[mid] == target){
-            return mid;
-        }
-        if(nums[lo] <= nums[mid]){
-            if(nums[lo] <= target && target <nums[mid]){
-                hi = mid - 1;
-            }else{
-                lo =  mid + 1;
-            }
-        }else{
-            if(target >= nums[mid+1] && target <=nums[hi]){
-                 lo = mid+1;
-            }else{
-                hi = mid -1;
-            }
-        }
-    }
-    return -1;
-    }
-}
+        while (lo <= hi) {  
+            int mid = lo + (hi - lo) / 2; // Prevents overflow  
+
+            if (nums[mid] == target) {  
+                return mid;  
+            }  
+
+            // Check if the left side is sorted  
+            if (nums[lo] <= nums[mid]) {  
+                // Target is in the left sorted portion  
+                if (nums[lo] <= target && target < nums[mid]) {  
+                    hi = mid - 1;  
+                } else {  
+                    lo = mid + 1;  
+                }  
+            } else { // Right side must be sorted  
+                // Check for target in the sorted right portion  
+                if (mid + 1 <= hi && target >= nums[mid + 1] && target <= nums[hi]) {  
+                    lo = mid + 1;  
+                } else {  
+                    hi = mid - 1;  
+                }  
+            }  
+        }  
+        return -1; // Target not found  
+    }  
+}  
